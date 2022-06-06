183dc0cc464279b8878cbb797d8530425dd632b3
Tony LaForgia <alforgia428@gmail.com>
2022-06-06 16:19:33 -0500

    added useRouter hook to newsId

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,7 +1,11 @@
+// useRouter is a custom hook created by the Next.js team
+import { useRouter } from "next/router";
+
 // our-domain.com/news/something-important
 // This is a dynamic page. The brackets [newsId] tells Next this is dynamic
 // A dynamic page means that anything after /news, will bring the user to this page
 function DetailPage() {
+  const router = useRouter();
   return <h1>The Detail Page</h1>;
 }
 
