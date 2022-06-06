5b28950dd4660f6fd714c8720f947cf44027d750
Tony LaForgia <alforgia428@gmail.com>
2022-06-06 16:24:22 -0500

    reviewed useRouter

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -6,6 +6,12 @@ import { useRouter } from "next/router";
 // A dynamic page means that anything after /news, will bring the user to this page
 function DetailPage() {
   const router = useRouter();
+
+  const newsId = router.query.newsId;
+
+  // send a request to backend API
+  // to fetch the news item with newsId
+
   return <h1>The Detail Page</h1>;
 }
 
