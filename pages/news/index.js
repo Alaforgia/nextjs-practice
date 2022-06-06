c838730b64b4f1cc12174bb475b76cd696a1a944
Tony LaForgia <alforgia428@gmail.com>
2022-06-06 16:26:39 -0500

    added Fragment to index.js

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,14 @@
 // our-domain.com/news
 
+import { Fragment } from "react";
+// Fragment allows us to use adjacent jsx elements
+
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>;
+    </Fragment>
+  );
 }
 
 export default NewsPage;
