7cafacd484aa3dd8a4b89976b88b6555dad1f501
Tony LaForgia <alforgia428@gmail.com>
2022-06-06 19:06:39 -0500

    replaced a tag with Link

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,16 +1,17 @@
 // our-domain.com/news
-
-import { Fragment } from "react";
+// Link is a sub-package that replaces <a> tag. Expects href.
+import Link from "next/link";
 // Fragment allows us to use adjacent jsx elements
+import { Fragment } from "react";
 
 function NewsPage() {
   return (
     <Fragment>
-      <h1>The News Page</h1>;
+      <h1>The News Page</h1>
       <ul>
         <li>
           {/* This is an HTML link construction */}
-          <a href="/news/nextjs-is-a-great-framework">NextJS Is A Great Framework</a>
+          <Link href="/news/nextjs-is-a-great-framework">NextJS Is A Great Framework</Link>
         </li>
         <li>Something Else</li>
       </ul>
